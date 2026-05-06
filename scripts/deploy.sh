4266854edf0a59c358b53e044875568ad0d914f9
Alan <alan.silva_noesis@prestador.globo>
2026-05-06 15:52:55 -0300

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+NAME="ghcr.io/alan-venv/rinha-service-2026"
 VERSION=`date +%s`
-TAG="ghcr.io/alan-venv/rinha-service-2026:${VERSION}"
+TAG="$NAME:${VERSION}"
 
 docker compose -f docker/docker-compose.yml down
-docker image remove $TAG
+docker rmi $(docker images --filter=reference="$NAME" -q)
 docker build -f docker/dockerfile --no-cache -t $TAG .
 
+clear
 read -rsp "GHCR_TOKEN: " GHCR_TOKEN
 echo
 printf '%s' "$GHCR_TOKEN" | docker login ghcr.io -u alan-venv --password-stdin
