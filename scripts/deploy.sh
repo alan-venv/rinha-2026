c36ff7abf997abf7c6b389f66b480d572c4bfc66
Alan <alan.silva_noesis@prestador.globo>
2026-05-06 10:10:12 -0300

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-TAG="ghcr.io/alan-venv/rinha-service-2026:latest"
+VERSION=`date +%s`
+TAG="ghcr.io/alan-venv/rinha-service-2026:${VERSION}"
 
 docker compose -f docker/docker-compose.yml down
 docker image remove $TAG
@@ -11,3 +12,6 @@ echo
 printf '%s' "$GHCR_TOKEN" | docker login ghcr.io -u alan-venv --password-stdin
 docker push $TAG
 docker logout ghcr.io
+
+clear
+echo "Version: $VERSION"
