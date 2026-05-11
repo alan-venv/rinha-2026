4771538c7a4b4f176394e54e618b28c6f4b1f6a6
Alan <alan.silva_noesis@prestador.globo>
2026-05-11 10:52:55 -0300

    wip

--- src/controller.rs
+++ src/controller.rs
@@ -1,9 +1,5 @@
-use actix_web::web::{Bytes, Json};
+use actix_web::web::Bytes;
 use actix_web::{HttpResponse, Responder, get, post};
-
-use crate::dto::ContentRequest;
-use crate::encoding;
-use crate::service;
 
 #[get("/ready")]
 pub async fn ready() -> impl Responder {
@@ -12,24 +8,25 @@ pub async fn ready() -> impl Responder {
 
 #[post("/fraud-score")]
 // recebe references por "Data" do actix via parametro aqui
-pub async fn score(request: Json<ContentRequest>) -> HttpResponse {
-    let vector = encoding::vectorization(request.0);
-    let fraud_score = service::fraud_score(&vector); // ,references.as_ref()
+pub async fn score() -> HttpResponse {
+    // let vector = encoding::vectorization(request.0);
+    // let fraud_score = service::fraud_score(&vector); // ,references.as_ref()
+
+    let fraud_score = 0.0;
 
     HttpResponse::Ok()
         .content_type("application/json")
-        .body(Bytes::from_static(static_response_body(fraud_score)))
+        .body(response(fraud_score))
 }
 
-fn static_response_body(fraud_score: f32) -> &'static [u8] {
-    SCORE_RESPONSES[(fraud_score * 5.0).round() as usize]
+fn response(fraud_score: f32) -> Bytes {
+    const RESPONSES: [&[u8]; 6] = [
+        br#"{"approved":true,"fraud_score":0.0}"#,
+        br#"{"approved":true,"fraud_score":0.2}"#,
+        br#"{"approved":true,"fraud_score":0.4}"#,
+        br#"{"approved":false,"fraud_score":0.6}"#,
+        br#"{"approved":false,"fraud_score":0.8}"#,
+        br#"{"approved":false,"fraud_score":1.0}"#,
+    ];
+    Bytes::from_static(RESPONSES[(fraud_score * 5.0).round() as usize])
 }
-
-const SCORE_RESPONSES: [&[u8]; 6] = [
-    br#"{"approved":true,"fraud_score":0.0}"#,
-    br#"{"approved":true,"fraud_score":0.2}"#,
-    br#"{"approved":true,"fraud_score":0.4}"#,
-    br#"{"approved":false,"fraud_score":0.6}"#,
-    br#"{"approved":false,"fraud_score":0.8}"#,
-    br#"{"approved":false,"fraud_score":1.0}"#,
-];
