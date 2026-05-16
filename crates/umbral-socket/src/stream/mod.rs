d730cede8ec7f1e2580ea540c1af94348313101b
Alan <alan.silva_noesis@prestador.globo>
2026-05-16 17:16:20 -0300

    external umbral-socket

--- crates/umbral-socket/src/stream/mod.rs
+++
@@ -1,7 +0,0 @@
-mod client;
-mod protocol;
-mod server;
-
-pub use client::UmbralClient;
-pub use protocol::{MethodId, REQUEST_HEADER_LEN, RESPONSE_HEADER_LEN, UmbralStatus};
-pub use server::UmbralServer;
