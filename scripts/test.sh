227035afa887e6d4169b9ff6d489b15e3f09f052
Alan <alan.silva_noesis@prestador.globo>
2026-05-16 16:01:13 -0300

    CP6

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-docker compose -f docker/docker-compose.yml down
-docker image remove ghcr.io/alan-venv/rinha-service-2026:test
-docker build -f docker/dockerfile -t ghcr.io/alan-venv/rinha-service-2026:test .
-docker compose -f docker/docker-compose.yml up -d
-k6 run scripts/k6/main.js
-docker compose -f docker/docker-compose.yml down
+docker compose -f docker/docker-compose.yml down > /dev/null 2>&1
+docker image remove ghcr.io/alan-venv/rinha-service-2026:test > /dev/null 2>&1
+docker build -f docker/dockerfile -t ghcr.io/alan-venv/rinha-service-2026:test . > /dev/null 2>&1
+docker compose -f docker/docker-compose.yml up -d > /dev/null 2>&1
+k6 run scripts/k6/main.js > /dev/null 2>&1
+docker compose -f docker/docker-compose.yml down > /dev/null 2>&1
 
-clear
 cat scripts/k6/results.json | grep -e "ms" -e "false_" -e "final" | tr -d '[:blank:]'
